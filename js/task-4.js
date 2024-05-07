fc22824a2d9d80c14ee545ec9d2e2028ff216e64
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-07 19:36:45 +0300

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector('.login-form');
 
+registerForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  const valueEmail = form.elements.email.value;
+  const valuePassword = form.elements.password.value;
+  let dataUser = {};
+
+  valueEmail.trim() === '' || valuePassword.trim() === ''
+    ? alert('All form fields must be filled in')
+    : (dataUser = {
+        email: valueEmail,
+        password: valuePassword,
+      });
+
+  console.log(dataUser);
+  form.reset();
+});
