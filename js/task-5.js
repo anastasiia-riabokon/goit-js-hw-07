997dd605063f259d150e5b32fdcd503fa3b34459
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-07 19:52:02 +0300

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('.widget');
+const color = document.querySelector('.color');
+const btnChange = document.querySelector('.change-color');
+
+btnChange.addEventListener('click', event => {
+  color.textContent = getRandomHexColor();
+  body.style.background = color.textContent;
+});
